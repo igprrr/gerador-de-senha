7f5b070187fbdb2d376e8dbfcddd963d628080a4
iqo <74800912+iguroo@users.noreply.github.com>
2021-08-29 01:14:21 -0300

    Add files via upload

---
+++ gerador de senha.py
@@ -0,0 +1,11 @@
+#gerador de senha
+import random
+chars = 'abcdefghijklmnopqrstuvwxyz'
+chars += chars.upper()
+num = str(1234567890)
+simb = '!@#$%^&*()_'
+chars += num + simb
+#tamanho
+tam = 8
+senha = ''.join(random.sample(chars, tam))
+print(senha)
